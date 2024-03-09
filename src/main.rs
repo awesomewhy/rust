e6b421a2443991c973ad1886c6169c07172bbcad
serenty <serenty395@gmail.com>
2024-03-09 01:22:56 +0000

    add some

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,10 @@ fn main() {
         .expect("ti chel..." );
 
     let result1 = some(123);
-    let result2 = some(1);
-    
+    //let result2 = some(1);
+    let arr = [1,2,3,4,5,6,7,8,9,10]
+    let res = binrySearch(5, arr)
+    println!("{res}")
     println!("{user_choise}");
     println!("{result1}");
     println!("{result2}");
@@ -28,6 +30,26 @@ fn some(y: i32) -> i32 {
 
 fn get() -> f32 {
     32.0
+}
+
+fn binrySearch(y: i32, arr: [i32]) -> i32{
+    let result = 0;
+    let right = arr.len()-1;
+    let left = arr[0];
+    while(left < right) {
+        int temp = left + (right - left) / 2;
+        if(temp == y) {
+            result = temp;
+            return temp;
+        }
+        if(y < temp) {
+            right--:
+        } else (
+            left++;
+        )
+        
+    }
+    return result;
 }
 
 /* fn get() -> String{
