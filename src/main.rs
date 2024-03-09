1c69d4aa42098e12b3396df0258a97bc05092650
serenty <serenty395@gmail.com>
2024-03-09 01:47:41 +0000

    add some

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
-const C: f32 = 23.0; 
 use std::io;
 
 
 fn main() {
 
-    let mut user_choise = String::new();
-
-    io::stdin().read_line(&mut user_choise).unwrap();
-    let temp = user_choise
-        .trim()
-        .parse::<u8>()
-        .expect("ti chel..." );
+    // let mut user_choise = String::new();
+    //
+    // io::stdin().read_line(&mut user_choise).unwrap();
+    // let temp = user_choise
+    //     .trim()
+    //     .parse::<u8>()
+    //     .expect("ti chel..." );
 
     let result1 = some(123);
-    //let result2 = some(1);
-    let arr = [1,2,3,4,5,6,7,8,9,10]
-    let res = binrySearch(5, arr)
-    println!("{res}")
-    println!("{user_choise}");
+    let arr = [1,2,3,4,5,6,7,8,9,10];
+    let res = binary_search(3, arr);
+    println!("{res}");
+    // println!("{user_choise}");
     println!("{result1}");
-    println!("{result2}");
+    println!("qwe");
+    println!("{res}");
 }
 
 fn some(y: i32) -> i32 {
     match y {
-        1 => y
+        1 => y,
+        _ => 1,
     }
 }
 
@@ -32,26 +32,25 @@ fn get() -> f32 {
     32.0
 }
 
-fn binrySearch(y: i32, arr: [i32]) -> i32{
-    let result = 0;
-    let right = arr.len()-1;
-    let left = arr[0];
-    while(left < right) {
-        int temp = left + (right - left) / 2;
-        if(temp == y) {
-            result = temp;
-            return temp;
+fn binary_search(y: i32, arr: [i32; 10]) -> i32 {
+    let mut result = -1;
+    let mut right = arr.len() as i32 - 1;
+    let mut left = 0;
+    while left <= right {
+        let mid = left + (right - left) / 2;
+        if arr[mid as usize] == y {
+            result = mid;
+            break;
         }
-        if(y < temp) {
-            right--:
-        } else (
-            left++;
-        )
-        
+        if arr[mid as usize] < y {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
     return result;
 }
 
-/* fn get() -> String{
-    return "qwe".toString();
-} */
+// fn getq() -> String{
+//     return "qwe".toString();
+// }
